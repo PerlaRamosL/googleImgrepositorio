06595f7f6dfa158b7d560ea3b5359930a5cedd05
PerlaRamosL <17106775.tecnmtlahuac.onmicrosoft.com>
2020-10-02 13:11:00 -0500

    primer commit

---
+++ contenido.php
@@ -0,0 +1,34 @@
+<?php 
+
+function contenido(){
+	$datos=array();
+
+	$datos[0]="img/atenaaa.jpg"."||".
+	          "Atenea"."||".
+	          "En la mitología griega, Atenea era una diosa guerrera, se le conferían los dones de la estrategia, ciencias y la habilidad.Nacida de Zeus,las historias cuentan que nacio de la cabeza de este, ya adulta y completamente armada.";
+
+	$datos[1]="img/Zeus.jpg"."||".
+	          "Zeus"."||".
+	          "En la mitología griega, Zeus, dios del trueno o el rayo, es padre de los dioses y los hombres. Zeus nació en la isla de creta donde fue escondidopor su madre Rea.
+              Al nacer, Cronos reclamó a su hijo recien nacido para engullirlo como al resto. Rea le entrgó una roca envuelta en pañales que cronos se comió sin dudad. Rea escondió a Zeus en una cueva de creta en donde fue criado y cuidado de diferente manera según cada versión de la historia.";
+
+	$datos[2]="img/hades.jpg"."||".
+	          "Hades"."||".
+	          "Hades es el dios del inframundo y los muertos. Hijo de los titanes Cronos y Rea, era el mayor de sus hijos varones.
+               Al igual que todos sus hermanos, fue devorado por su padre y posteriormente rescatado por Zeus. Hades junto a Zeus y Poseidón, lograron derrotar a Cronos y se adueñaron del universo.";
+
+	$datos[3]="img/poseidon.jpg"."||".
+	          "Poseidón"."||".
+	          "Dios de los mares y los océanos, es el regente de las aguas y creador de los terremotos. Devorado al nacer por su padre Cronos, fue salvado por su hermano Zeus. Algunas historias cuentan que al igual que su hermano, Poseidón no fue devorado por Cronos,si no que fue escondido por su madre dentro de un rebaño de corderos."; 
+	          
+	$datos[4]="img/afrodita.jpg"."||".
+	          "Afrodita"."||".
+	          "En la mitología griega, Afrodita es la diosa de la belleza y la lujuria, la pasión y todo lo relacionado al sexo y la reproducción.
+	          Nacida del esperma de Urano, al que le fueron cortados los testículos por Cronos, Afrodita nació en el mar siendo una adulta e inmediatamente fue deseada por muchos dioses.";                            
+
+ 
+  return $datos;
+}
+ 
+
+ ?>
